6a8fe87f3f19f15cec1ea1a7fc0ec8f0b78644b5
Bastien Winant <babw1@student.london.ac.uk>
2023-05-21 22:05:02 +0200

    add score reset button

--- WebDevBasics/BasketballScoreboard/js/index.js
+++ WebDevBasics/BasketballScoreboard/js/index.js
@@ -4,13 +4,35 @@ function initializeScores() {
   for (const score of scores) {
     score.textContent = 0;
   }
+
+  highlightLeader()
 }
 
 function increaseScore(element) {
   let points = parseInt(element.textContent);
   let scoreEl = element.parentElement.previousElementSibling;
-  
+
   scoreEl.textContent = parseInt(scoreEl.textContent) + points;
+
+  highlightLeader();
+}
+
+function highlightLeader() {
+  let scores = document.getElementsByClassName("score");
+
+  let x = parseInt(scores[0].textContent);
+  let y = parseInt(scores[1].textContent);
+
+  if (x > y) {
+    scores[0].style.background = "#9AABD855";
+    scores[1].style.background = "#FFFFFF";
+  } else if (y > x) {
+    scores[0].style.background = "#FFFFFF";
+    scores[1].style.background = "#9AABD855";
+  } else {
+    scores[0].style.background = "#FFFFFF";
+    scores[1].style.background = "#FFFFFF";
+  }
 }
 
 initializeScores()
