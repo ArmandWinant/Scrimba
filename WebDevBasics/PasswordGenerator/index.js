aafbd4652eac1ff5db1d530ca99e2e7e3e9c7ac1
Bastien Winant <babw1@student.london.ac.uk>
2023-06-22 08:59:01 +0200

    add password generating functions

--- WebDevBasics/PasswordGenerator/index.js
+++ WebDevBasics/PasswordGenerator/index.js
@@ -1,1 +1,27 @@
-console.log("cjsadlvnasjkvn")
+function newPassword(n) {
+  const characters = [
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-",
+    "+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"]
+    
+  let password = ""
+
+  for (let i = 0; i < n; i++) {
+    let nextCharIndex = Math.floor(Math.random() * characters.length)
+    let nextChar = characters[nextCharIndex];
+
+    password += nextChar
+  }
+
+  return password
+}
+
+function generatePasswords() {
+  let passwordEls = document.getElementsByClassName('password')
+  console.log(passwordEls.length)
+
+  for (let i = 0, n = passwordEls.length; i < n; i++) {
+    passwordEls[i].textContent = newPassword(15);
+  }
+}
